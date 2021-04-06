14532b8cb1d9c25637a02bd705403e65b637e68c
Romaric Mourgues <rmourgues@linagora.com>
2021-04-06 09:29:29 +0200

    Add Jitsi and LinShare connectors

--- connectors/R7/Resources/Services.php
+++ connectors/R7/Resources/Services.php
@@ -7,7 +7,7 @@ use Common\BaseServices;
 class Services extends BaseServices
 {
     protected $services = [
-      "connectors.r7.main" => "Main"
+      "connectors.r7.main" => "Main",
       "connectors.r7.event" => "Event"
     ];
 }
